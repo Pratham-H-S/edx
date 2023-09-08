466bf1076aaaa1865e01d28db3d9acfc204dadcf
Replit user <>
2023-09-08 03:54:43 +0000

    initial changes

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, render_template,redirect ,request,send_file,session, url_for , g ,Response
+
+app = Flask(__name__,
+           	template_folder='templates',  
+ 	          static_folder='static' 
+           )
+@app.route("/",methods = ['GET','POST'])
+
+def home():
+  return render_template("index.html")
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',port = 5000,debug= True)
