45c964645ea7f03affcb413c8e55695aae7d667f
Replit user <>
2023-09-10 16:29:23 +0000

    new changes

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__,
  	          static_folder='static' 
            )
 @app.route("/",methods = ['GET','POST'])
-
 def home():
   return render_template("index.html")
+
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port = 5000,debug= True)
